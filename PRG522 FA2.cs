329b4a655243a77599091a592f4dcac99f9bbddf
PrajnenM <80842943+PrajnenM@users.noreply.github.com>
2021-08-16 20:05:44 +0200

    Add files via upload

---
+++ PRG522 FA2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PRG522_FA2_8025_S2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
